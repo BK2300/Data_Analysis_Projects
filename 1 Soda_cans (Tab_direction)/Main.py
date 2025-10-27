efd1be779df49e7f695ee54bb169a71536167bb0
BK2300 <BK2300@yahoo.com>
2025-10-27 08:22:27 +0100

    playing around with ideas

--- 1 Soda_cans (Tab_direction)/Main.py
+++ 1 Soda_cans (Tab_direction)/Main.py
@@ -5,33 +5,39 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("Sodacan DF.csv")
 
 
-print(df.describe())                  # shows the summary of numerical data
-print(df.describe(include=["object"]))# shows the summary of categorical data
+print(df.describe())                      # shows the summary of numerical data
+print(df.describe(include=["object"]))    # shows the summary of categorical data
+
+def visualize_histogram(df):
+    # I wanted to start out seen the distribution of the sodacan angle.
+    counts = df["Angle"].dropna().value_counts().sort_index()
+
+    # Draw histogram
+    ax = counts.plot(kind="bar")
+
+    # Labels & titles
+    ax.set_title("Distribution of Angle")
+    ax.set_xlabel("Angle")
+    ax.set_ylabel("Count")
+
+    # access the number on top of the histogram
+    for i, v in enumerate(counts.values):
+        ax.text(i, v, str(v), ha="center", va="bottom")
+
+    plt.show()
+    return plt
+
+visualize_histogram(df)
 
 
-# 2: I wanted to start out seen the distribution of the sodacan angle.
-counts = df["Angle"].dropna().value_counts().sort_index()
 
-# 2) Tegn søjlediagram
-ax = counts.plot(kind="bar")
 
-# 3) Labels og titel
-ax.set_title("Distribution of Angle")
-ax.set_xlabel("Angle")
-ax.set_ylabel("Count")
-
-# 4) Skriv tallet oven på hver søjle
-for i, v in enumerate(counts.values):
-    ax.text(i, v, str(v), ha="center", va="bottom")
-
-plt.show()
-
-df["Angle"].value_counts().sort_index().plot(kind="bar", edgecolor="black")
+""" df["Angle"].value_counts().sort_index().plot(kind="bar", edgecolor="black")
 plt.title("Distribution of Angle")
 plt.xlabel("Angle")
 plt.ylabel("Count")
 plt.show()
-
+"""
 
 
 
