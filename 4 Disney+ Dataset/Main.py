b5ca0422adc315309cd9be6f201c0b87913fe5f6
BK2300 <BK2300@yahoo.com>
2025-11-21 09:55:39 +0100

    something random ideas

---
+++ 4 Disney+ Dataset/Main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("disney_plus_titles.csv")
+
+
+print(df.head())
+print(df.columns)
+print(df.describe())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
