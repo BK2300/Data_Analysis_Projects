bc08516dcb4117fdb9276b131ef4b22b06bfb888
BK2300 <BK2300@yahoo.com>
2026-01-31 22:53:18 +0100

    Netflix project with dash

--- 3 Netflix/Analysis plots.py
+++ 3 Netflix/Analysis plots.py
@@ -1,14 +1,16 @@
+# This tab is for the basic EDA functions with plotly
+
 import plotly.express as px
+import pandas as pd
 
 
-
-def hist_show_spread():
+def hist_show_spread(df: pd.Dataframe):
+# This function is a simple histogram, showing the dpread between "TV-shows" and "Movies"
     df = px.data.tips()
-    # Here we use a column with categorical data
     fig = px.histogram(df, x="day")
     fig.show()
 
-
+def
 
 
 
